435e9546ed7bab080a79bf16d16041a5656a382e
William Tumeo <tumeowilliam@gmail.com>
2017-01-21 09:48:12 -0200

    Add gravitational wave transition

---
+++ scripts/gameplay/GravitationalWave.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+export var wave_size = 1
+export(bool) var wave_enabled setget _wave_enabled
+onready var tween = get_node("Tween")
+
+func _ready():
+	pass
+
+func _wave_enabled(val):
+	wave_enabled = val
+	if val:
+		wave(wave_size)
+
+func wave(size):
+	print(size)
+	var property = 'transform/scale'
+	var wave_size_ = get_scale() + (Vector2(1, 1) * size * 10)
+	tween.interpolate_property(self, property, get_scale(), wave_size_, 3, tween.TRANS_LINEAR, tween.EASE_IN)
+	tween.start()
